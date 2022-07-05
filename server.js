cbeb7775672fdc28a1d33ac4a4c5214cd77e0769
sushantyerawar1 <84599862+sushantyerawar1@users.noreply.github.com>
2022-07-05 18:36:29 +0530

    crud

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const morgan = require("morgan");
+const path = require("path");
+const ejs = require("ejs");
+const bodyparser = require("body-parser");
+const { dirname } = require("path");
+
+const connectDB = require("./server/database/connection");
+
+const app = express();
+
+
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
+
+
+// log requests 
+app.use(morgan("tiny"));
+
+
+//MongoDB Connection
+connectDB();
+
+// parse request to body parser 
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine 
+app.set("view engine", 'ejs');
+// app.set(path.resolve(__dirname, "views/ejs"));
+
+// load assets 
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+//css/style.css
+
+// load routers 
+app.use('/', require('./server/routes/router'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
